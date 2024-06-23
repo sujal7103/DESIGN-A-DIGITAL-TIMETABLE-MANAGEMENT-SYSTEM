aec456933ad2fc1e9690f57b07cf196ac3e0e40b
sujal7103 <150238326+sujal7103@users.noreply.github.com>
2024-06-24 01:41:32 +0530

    Delete DIGITAL TIMETABLE MANAGEMENT SYSTEM directory

--- DIGITAL TIMETABLE MANAGEMENT SYSTEM/Professor.java
+++
@@ -1,7 +0,0 @@
-import java.util.*; 
-public class Professor extends Timetable{
-    private ArrayList<String> courses = new ArrayList<String>();
-    void addCourses(String s){
-        this.courses.add(s);
-    }
-}
